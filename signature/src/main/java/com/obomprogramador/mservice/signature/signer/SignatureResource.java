7cbd1b50c76693425e646c7e3cf2b2c5b3b98a9d
cleuton <cleuton.sampaio@gmail.com>
2015-04-30 13:50:14 -0300

    Mudei a mensagem para "OK"

--- signature/src/main/java/com/obomprogramador/mservice/signature/signer/SignatureResource.java
+++ signature/src/main/java/com/obomprogramador/mservice/signature/signer/SignatureResource.java
@@ -31,7 +31,7 @@ public class SignatureResource {
 	    	}
 	    	else {
 	    		status = "success";
-	    		mensagem = "assinatura ok, certo!";    		
+	    		mensagem = "assinatura ok!";    		
 	    	}
 		} catch (Exception e) {
 			status = "error";
